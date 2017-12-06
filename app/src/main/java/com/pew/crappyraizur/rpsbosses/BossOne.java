1313d4b20a16f08379da9cb94d39f156c722be83
CrappyRaizur <razizo41@hotmail.com>
2017-12-06 02:21:22 -0700

    added more stuff and boss page basis

--- app/src/main/java/com/pew/crappyraizur/rpsbosses/BossOne.java
+++ app/src/main/java/com/pew/crappyraizur/rpsbosses/BossOne.java
@@ -2,10 +2,21 @@ package com.pew.crappyraizur.rpsbosses;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BossOne extends AppCompatActivity {
     //Rock paper scizzah against boss
     //TODO: implement boss class and use it for the user to fight the boss
+
+    TextView userScore;
+    TextView bossScore;
+
+    TextView userChoice;
+    TextView bossChoice;
+
+    TextView gameState;
+
+
 
 
     @Override
---
+++ app/src/main/java/com/pew/crappyraizur/rpsbosses/BossTwo.java
@@ -0,0 +1,13 @@
+package com.pew.crappyraizur.rpsbosses;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BossTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boss_two);
+    }
+}
