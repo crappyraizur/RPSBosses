8519a9a958512af21c1441e218bfc64977ac51c7
CrappyRaizur <razizo41@hotmail.com>
2017-12-05 20:58:59 -0700

    added basis for storing user score and progression

--- app/src/main/java/com/pew/crappyraizur/rpsbosses/MainMenu.java
+++ app/src/main/java/com/pew/crappyraizur/rpsbosses/MainMenu.java
@@ -1,13 +1,50 @@
 package com.pew.crappyraizur.rpsbosses;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class MainMenu extends AppCompatActivity {
 
+    Button singleplayer;
+    Button multiplayer;
+    Button about;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        singleplayer = (Button) findViewById(R.id.singleplayer);
+        multiplayer = (Button) findViewById(R.id.multiplayer);
+        about = (Button) findViewById(R.id.button7);
+
+
+
+    }
+
+    public void storePreference(HashMap<String, String> parameters) {
+        // Context context = this.SignUpPage;
+
+        SharedPreferences.Editor editor =  getApplicationContext().getSharedPreferences("User", MODE_PRIVATE)
+                .edit();
+        Iterator<Map.Entry<String, String>> it = parameters.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, String> pairs = it.next();
+            editor.putString((String) pairs.getKey(), (String) pairs.getValue());
+        }
+        editor.commit();
+    }
+
+    public  HashMap<String, String> getPreference(String[] keys) {
+        SharedPreferences sharedPref = getSharedPreferences("User", MODE_PRIVATE);
+        HashMap<String, String> parameters = new HashMap<String, String>();
+        for (String key : keys) {
+            parameters.put(key, sharedPref.getString(key, null));
+        }
+        return parameters;
     }
 }
