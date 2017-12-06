603c962583aafcb0c09dea437a49d8085ed80ba8
CrappyRaizur <razizo41@hotmail.com>
2017-12-06 02:28:53 -0700

    added more stuff and boss page basis

--- app/src/main/java/com/pew/crappyraizur/rpsbosses/BossOne.java
+++ app/src/main/java/com/pew/crappyraizur/rpsbosses/BossOne.java
@@ -2,6 +2,7 @@ package com.pew.crappyraizur.rpsbosses;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class BossOne extends AppCompatActivity {
@@ -16,12 +17,28 @@ public class BossOne extends AppCompatActivity {
 
     TextView gameState;
 
+    Button rock;
+    Button paper;
+    Button scissor;
 
-
+    BossClass boss = new BossClass(1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boss_one);
+
+        userScore = findViewById(R.id.userScore);
+        bossScore = findViewById(R.id.bossScore);
+
+        userChoice = findViewById(R.id.userChoice);
+        bossChoice = findViewById(R.id.userChoice);
+
+        gameState = findViewById(R.id.GameState);
+
+        rock = findViewById(R.id.rock);
+        paper = findViewById(R.id.paper);
+        scissor = findViewById(R.id.scissor);
+
     }
 }
